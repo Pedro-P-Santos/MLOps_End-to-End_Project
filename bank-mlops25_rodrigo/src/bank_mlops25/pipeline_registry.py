699cb9c2ef016500cd22c5828f34820dbcbe06c1
Rodrigo <rodrigo.miranda0303@gmail.com>
2025-06-17 16:24:16 +0100

    copia

---
+++ bank-mlops25_rodrigo/src/bank_mlops25/pipeline_registry.py
@@ -0,0 +1,30 @@
+"""Project pipelines."""
+from __future__ import annotations
+
+from kedro.pipeline import Pipeline, pipeline
+from typing import Dict
+from bank_mlops25.pipelines import (
+    data_tests,
+    split_data,
+    preprocess,
+    model_selec
+)
+
+def register_pipelines() -> dict[str, Pipeline]:
+    """Register the project's pipelines.
+
+    Returns:
+        A mapping from pipeline names to ``Pipeline`` objects.
+    """
+    data_tests_pipeline = data_tests.create_pipeline()
+    split_data_pipeline = split_data.create_pipeline()
+    preprocess_pipeline=preprocess.create_pipeline()
+    model_selec_pipeliene = model_selec.create_pipeline()
+
+    return {
+        "data_tests": data_tests_pipeline,
+        "split_data":split_data_pipeline,
+        "preprocess": preprocess_pipeline,
+        "model_selec": model_selec_pipeliene
+
+    }
