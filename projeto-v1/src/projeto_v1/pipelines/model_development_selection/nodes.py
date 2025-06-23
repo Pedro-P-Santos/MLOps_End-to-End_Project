b15b8282bed9bb7d6329dd10de9b997cbed43656
Pedro Santos <20240295@novaims.unl.pt>
2025-06-23 11:30:23 +0100

    WIP

--- projeto-v1/src/projeto_v1/pipelines/model_development_selection/nodes.py
+++ projeto-v1/src/projeto_v1/pipelines/model_development_selection/nodes.py
@@ -18,7 +18,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import f1_score, recall_score, precision_score
 from sklearn.preprocessing import StandardScaler
 import optuna
-
+from optuna.samplers import TPESampler
 
 
 logger = logging.getLogger(__name__)
@@ -41,29 +41,37 @@ def model_selection(
     seed_everything(seed)
     
     models_dict = define_models(seed)
+
     X_train_scaled, X_test_scaled = scale_features(
         X_train_preprocessed[final_selected_features],
         X_test_preprocessed[final_selected_features]
     )
     
-    best_model, best_name, best_f1, best_model_class = train_base_models(
+    best_model, best_name, best_f1_base, best_model_class = train_base_models(
     models_dict, X_train_scaled, y_train_encoded, X_test_scaled, y_test_encoded, experiment_name
     )
     
-    champion_model = run_grid_search(
-    best_model=best_model,
-    best_model_class=best_model_class,
-    best_name=best_name,
-    best_f1_base=best_f1,
-    X_train=X_train_scaled,
-    y_train=y_train_encoded,
-    X_test=X_test_scaled,
-    y_test=y_test_encoded,
-    parameters_grid=parameters_grid,
-    seed=seed
+    tuned_model, optuna_f1 = run_optuna_for_all_models(
+        X_train=X_train_scaled,
+        y_train=y_train_encoded,
+        X_test=X_test_scaled,
+        y_test=y_test_encoded,
+        parameters_grid=parameters_grid,
+        seed=seed
     )
 
     register_model(champion_model, best_name, model_name)
+
+    champion_model = compare_models(
+        base_model=best_model,
+        final_model=tuned_model,
+        base_f1=best_f1_base,
+        tuned_f1=optuna_f1,
+        name=best_name,
+        register_model=True,
+        model_name="ChampionModel"
+    )
+
     return champion_model
 
 
@@ -97,12 +105,12 @@ def train_base_models(models_dict, X_train, y_train, X_test, y_test, experiment_
     mlflow.set_experiment(experiment_name)
     logger.info("Phase 1: Training base models and logging to MLflow...")
     results = {}
-    valid_models = {}
+    # valid_models = {}
 
     for name, model in models_dict.items():
         with mlflow.start_run(run_name=f"{name}_V2", nested=True):
-            mlflow.set_tag("stage", "Tuned")
-            mlflow.set_tag("Round", "3")
+            mlflow.set_tag("stage", "Optuna")
+            mlflow.set_tag("Round", "4")
             mlflow.set_tag("Model_type", name)
 
             mlflow.set_tag("mlflow.note.content", "Hyperparameter Tuning, with 10 perc. control overfitting check.")
@@ -128,17 +136,14 @@ def train_base_models(models_dict, X_train, y_train, X_test, y_test, experiment_
             mlflow.log_metric("recall_macro", recall)
             mlflow.log_metric("precision_macro", precision)
             
-            if f1_diff < 0.1:
-                valid_models[name] = (model, f1)
-                
-    if valid_models:
-        best_name = max(valid_models, key=lambda k: valid_models[k][1])
-        best_model = valid_models[best_name][0]
-        best_f1 = valid_models[best_name][1]
+            # if f1_diff < 0.1:
+            #     valid_models[name] = (model, f1)
+        results[name] =  (model,f1)
+
+        best_name = max(results, key=lambda k: results[k][1])
+        best_model = results[best_name][0]
+        best_f1 = results[best_name][1]
         logger.info(f"Best valid model: {best_name} | F1_macro: {best_f1:.4f}")
-    else:
-        best_model, best_name, best_f1 = None, None, None
-        logger.warning(" No valid models found with f1_diff < 0.1")
 
     return best_model, best_name, best_f1, best_model.__class__
 
@@ -154,141 +159,169 @@ def register_model(model, name:str, model_name: str):
             name=model_name
         )
 
-# ## GRID
-def run_grid_search(
-    best_model,
-    best_model_class,
-    best_name,
-    best_f1_base,
+
+
+## OPTUNA ##
+def run_optuna_for_all_models(
     X_train,
     y_train,
     X_test,
     y_test,
     parameters_grid,
-    seed
+    seed,
+    n_trials= 5
 ):
-    logger.info(f"🔍 Phase 2: Running GridSearchCV on {best_name}...")
+    candidates = ["RandomForestClassifier", "GradientBoostingClassifier", "XGBClassifier", "KNeighborsClassifier"]
+    results = {}
 
-    param_grid = parameters_grid["hyperparameters"][best_name]
+    for name in candidates:
+        model = run_optuna_search(
+            best_name=name,
+            X_train=X_train,
+            y_train=y_train,
+            X_test=X_test,
+            y_test=y_test,
+            parameters_grid=parameters_grid,
+            seed=seed,
+            n_trials=n_trials
+        )
+        preds = model.predict(X_test)
+        f1 = f1_score(y_test, preds, average="macro")
+        results[name] = (model, f1)
 
-    try:
-        base_estimator = best_model_class(random_state=seed)
-    except TypeError:
-        base_estimator = best_model_class()
-
-    grid_model = GridSearchCV(
-        estimator=base_estimator,
-        param_grid=param_grid,
-        scoring="f1_macro",
-        n_jobs=-1,
-        cv=3
-    )
-
-    with mlflow.start_run(run_name=f"{best_name}_gridsearch", nested=True):
-        mlflow.set_tag("stage", "Tuned")
-        mlflow.set_tag("Round", "3")
-        mlflow.set_tag("Model_type", best_name)
-        mlflow.sklearn.autolog()
-        
-
-        grid_model.fit(X_train, np.ravel(y_train))
-        tuned_model = grid_model.best_estimator_
-
-        preds = tuned_model.predict(X_test)
-        best_f1_tuned = f1_score(y_test, preds, average="macro")
-
-        mlflow.log_params(grid_model.best_params_)
-        logger.info(f"Tuned {best_name} | F1_macro: {best_f1_tuned:.4f}")
-
-        if best_f1_tuned > best_f1_base:
-            logger.info(f"🎯 Champion: Tuned {best_name} selected.")
-            return tuned_model
-        else:
-            logger.info(f"⚖️ Champion: Base {best_name} retained.")
-            return best_model
+    best_name = max(results, key=lambda k: results[k][1])
+    best_model = results[best_name][0]
+    logger.info(f"🏆 Best Optuna Model: {best_name} | F1_macro: {results[best_name][1]:.4f}")
+    return best_model, best_name
 
 
-
-# ## OPTUNA ##
-# def optuna_objective(trial: optuna.Trial, model_name: str, X_train, X_test, y_train, y_test) -> float:
-#     if model_name == "RandomForestClassifier":
-#         model = RandomForestClassifier(
-#             n_estimators=trial.suggest_categorical("n_estimators", [100, 200, 300]),
-#             max_depth=trial.suggest_categorical("max_depth", [5, 6, 7, 8]),
-#             random_state=2021,
-#         )
-#     elif model_name == "GradientBoostingClassifier":
-#         model = GradientBoostingClassifier(
-#             n_estimators=trial.suggest_categorical("n_estimators", [100, 200, 300]),
-#             max_depth=trial.suggest_categorical("max_depth", [4, 5, 6, 7]),
-#             learning_rate=trial.suggest_categorical("learning_rate", [0.05, 0.1, 0.01]),
-#             random_state=2021,
-#         )
-#     elif model_name == "XGBClassifier":
-#         model = XGBClassifier(
-#             n_estimators=trial.suggest_categorical("n_estimators", [100, 200, 300]),
-#             max_depth=trial.suggest_categorical("max_depth", [4, 5, 6, 7]),
-#             learning_rate=trial.suggest_categorical("learning_rate", [0.05, 0.1, 0.01]),
-#             use_label_encoder=False,
-#             eval_metric="logloss",
-#             random_state=2021,
-#         )
-#     else:
-#         raise ValueError("Unsupported model")
-
-#     model.fit(X_train, np.ravel(y_train))
-#     preds = model.predict(X_test)
-#     f1 = f1_score(y_test, preds, average="macro")
-
-#     mlflow.log_params(trial.params)
-#     mlflow.log_metric("f1_macro", f1)
-
-#     return f1
-
-
-#  def run_optuna_optimization(X_train, X_test, y_train, y_test, model_name: str, n_trials: int = 25):
-#     mlflow.set_experiment("Optuna_Tuning")
+def run_optuna_search(
+        best_name: str,
+        X_train,
+        y_train,
+        X_test,
+        y_test,
+        parameters_grid: Dict[str,Any],
+        seed: int,
+        n_trials: int = 5
+):
+    logger.info(f"Moved on from Classical GridSearch to Optuna -> Phase 4.")
+    logger.info(f"Running Optuna on {best_name}")
     
-#     with mlflow.start_run(run_name=f"{model_name}_Optuna_R2"):
-#         mlflow.set_tag("stage", "Tuned")
-#         mlflow.set_tag("Round", "2")
-#         mlflow.set_tag("Model_type", model_name)
-        
-#         study = optuna.create_study(direction="maximize")
-#         study.optimize(
-#             lambda trial: optuna_objective(trial, model_name, X_train, X_test, y_train, y_test),
-#             n_trials=n_trials
-#         )
 
-#         best_params = study.best_params
-#         best_score = study.best_value
-#         mlflow.log_params(best_params)
-#         mlflow.log_metric("best_f1_macro", best_score)
+    def objective(trial):
+        if best_name == "RandomForestClassifier":
+            params = {
+                "n_estimators": trial.suggest_categorical("n_estimators", [150, 200, 250, 300, 350, 400]),
+                "max_depth": trial.suggest_categorical("max_depth", [5, 6, 7, 8]),
+                "n_jobs": trial.suggest_categorical("n_jobs", [-1]),
+                "class_weight": trial.suggest_categorical("class_weight", ["balanced", "balanced_subsample"]),
+            }
+            model = RandomForestClassifier(**params)
 
-#         print(f"🏆 Best F1_macro: {best_score:.4f}")
-#         print(f"Best Params: {best_params}")
+        elif best_name == "GradientBoostingClassifier":
+            params = {
+                "n_estimators": trial.suggest_categorical("n_estimators", [150, 200, 250, 300, 350, 400]),
+                "max_depth": trial.suggest_categorical("max_depth", [4, 5, 6, 7]),
+                "learning_rate": trial.suggest_categorical("learning_rate", [0.05, 0.1, 0.01]),
+                "random_state": seed
+            }
+            model = GradientBoostingClassifier(**params)
 
-#         return best_params, best_score
+        elif best_name == "XGBClassifier":
+            params = {
+                "n_estimators": trial.suggest_categorical("n_estimators", [150, 200, 250, 300, 350, 400]),
+                "max_depth": trial.suggest_categorical("max_depth", [4, 5, 6, 7]),
+                "learning_rate": trial.suggest_categorical("learning_rate", [0.05, 0.1, 0.01]),
+                "use_label_encoder": False,
+                "eval_metric": "logloss",
+                "random_state": seed
+            }
+            model = XGBClassifier(**params)
+
+        elif best_name == "KNeighborsClassifier":
+            params = {
+                "n_neighbors": trial.suggest_categorical("n_neighbors", [3, 4, 5 ,6 , 7, 8]),
+                "weights": trial.suggest_categorical("weights", ["uniform", "distance"]),
+                "metric": trial.suggest_categorical("metric", ["minkowski"])
+            }
+            model = KNeighborsClassifier(**params)
+
+        else:
+            raise ValueError(f"Optuna not implemented for model: {best_name}")
+
+        model.fit(X_train, np.ravel(y_train))
+        preds = model.predict(X_test)
+        f1_macro = f1_score(y_test, preds, average="macro")
+        return f1_macro
+    
+    study = optuna.create_study(direction="maximize", sampler=TPESampler(seed=seed))
+    with mlflow.start_run(run_name=f"{best_name}_optuna", nested=True):
+
+        mlflow.set_tag("stage", "Optuna")
+        mlflow.set_tag("Round", "4")
+        mlflow.set_tag("Model_type", best_name)
+        mlflow.set_tag("mlflow.note.content", "OPTUNA RUN")
+
+        study.optimize(objective, n_trials=n_trials, show_progress_bar=True)
+
+        best_params = study.best_trial.params
+        mlflow.log_params(best_params)
+
+        logger.info(f" Best Params via Optuna: {best_params}")
+
+        # Final model training
+        if best_name == "RandomForestClassifier":
+            final_model = RandomForestClassifier(**best_params, random_state=seed)
+        elif best_name == "GradientBoostingClassifier":
+            final_model = GradientBoostingClassifier(**best_params, random_state=seed)
+        elif best_name == "XGBClassifier":
+            final_model = XGBClassifier(**best_params, use_label_encoder=False, eval_metric="logloss", random_state=seed)
+        elif best_name == "KNeighborsClassifier":
+            final_model = KNeighborsClassifier(**best_params)
+
+        final_model.fit(X_train, np.ravel(y_train))
+        preds = final_model.predict(X_test)
+        f1 = f1_score(y_test, preds, average="macro")
+        mlflow.log_metric("optuna_f1_macro", f1)
+
+    return final_model, f1
+
+
+
+
    
 
 
 
-# def compare_models(base_model, tuned_model, base_f1, tuned_f1, name, register_model: bool = True, model_name: str = "_"):
-#    if tuned_f1 > base_f1:
-#         logger.info(f"Champion: Tuned {name} (F1_macro: {tuned_f1:.4f}) > Base (F1_macro: {base_f1:.4f})")
-#         champion_model = tuned_model
-#     else:
-#         logger.info(f"Champion: Base {name} (F1_macro: {base_f1:.4f}) retained over Tuned (F1_macro: {tuned_f1:.4f})")
-#         champion_model = base_model
+def compare_models(
+    base_model,
+    final_model,
+    base_f1: float,
+    tuned_f1: float,
+    name: str,
+    register_model: bool = True,
+    model_name: str = "_"
+):
+    """
+    Compare base and tuned models using F1 score and register the best (champion) to MLflow.
+    """
 
-#     if register_model:
-#         with mlflow.start_run(run_name=f"{name}_register", nested=True) as run:
-#             logger.info(f"📦 Logging and registering model {model_name}...")
-#             mlflow.sklearn.log_model(champion_model, artifact_path="model")
+    if tuned_f1 > base_f1:
+        logger.info(f"Champion: Tuned {name} selected (F1_macro: {tuned_f1:.4f} > {base_f1:.4f})")
+        champion_model = final_model
+    else:
+        logger.info(f"Champion: Base {name} retained (F1_macro: {base_f1:.4f} ≥ {tuned_f1:.4f})")
+        champion_model = base_model
 
-#             mlflow.register_model(
-#                 model_uri=f"runs:/{run.info.run_id}/model",
-#                 name=model_name
-#             )
+    if register_model:
+        with mlflow.start_run(run_name=f"{name}_register", nested=True) as run:
+            logger.info(f"Logging and registering model to MLflow: {model_name}")
+            mlflow.sklearn.log_model(champion_model, artifact_path="model")
 
-#     return champion_model
+            mlflow.register_model(
+                model_uri=f"runs:/{run.info.run_id}/model",
+                name=model_name
+            )
+
+    return champion_model
